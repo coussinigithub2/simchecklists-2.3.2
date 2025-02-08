8d9fce73c584f3df2a47ff43669c0776f1c81ffc
coussinigithub <Coussini@Gmail.Com>
2025-02-08 13:11:30 -0500

    big change

--- js/wakelock.js
+++
@@ -1,6 +0,0 @@
-wakeLock = navigator.wakeLock.request('screen');
-window.onfocus = function() { wakeLock = navigator.wakeLock.request('screen'); }
-
-window.onblur = function() { 
-	// Need to Release? 
-}
